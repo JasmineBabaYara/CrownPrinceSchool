3303534d5ddd439681cf49a3deb7fcfeb24800da
JasmineBabaYara <babayarajasmine@gmail.com>
2025-04-09 14:37:22 +0000

    navigation

---
+++ components/navigation/page.js
@@ -0,0 +1,47 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
+
+export default function Navigation() {
+  const pathname = usePathname();
+  const [isScrolled, setIsScrolled] = useState();
+
+  //function to determine whether screen is being scrolled
+  useEffect(() => {
+    //disable scroll on menu open
+    if (open === true) {
+      document.querySelector('body').style.overflow = 'hidden';
+    } else {
+      document.querySelector('body').style.overflow = 'scroll';
+    }
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const shouldAddBackground = scrollPosition > 0;
+
+      //if screen is moved add background color
+      setIsScrolled(shouldAddBackground);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [open]);
+  return (
+    <div className={`fixed z-10 w-full h-32 bg-transparent flex items-center justify-center p-10 navbar ${isScrolled ? "scrolled" : ""}`}>
+      <div>
+        <p className='uppercase text-7xl text-black ml-20 mr-20' style={{ fontFamily: 'nautilus_pompilius' }}>CPS</p>
+      </div>
+      <div className='flex gap-x-10'>
+        <Link href="/" className={`uppercase text-base text-black relative ${pathname === '/' ? 'active' : ''}`}>Home</Link>
+        <Link href="#about" className={`uppercase text-base text-black relative ${pathname === '#about' ? 'active' : ''}`}>About Us</Link>
+        <Link href="#menu" className={`uppercase text-base text-black relative ${pathname === '#menu' ? 'active' : ''}`}>junior school</Link>
+        <Link href="#gallery" className={`uppercase text-base text-black relative ${pathname === '#gallery' ? 'active' : ''}`}>senior school</Link>
+        <Link href="#order" className={`uppercase text-base text-black relative ${pathname === '/order' ? 'active' : ''}`}>admissions </Link>
+        <Link href="/contact" className={`uppercase text-base text-black relative ${pathname === '/contact' ? 'active' : ''}`}>Contact Us</Link>
+      </div>
+    </div>
+  );
+}
