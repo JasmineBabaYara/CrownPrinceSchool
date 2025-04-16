a04944f8903c8e9a813f0621a2c0071cf1f43c0b
JasmineBabaYara <babayarajasmine@gmail.com>
2025-04-16 16:11:42 +0000

    rnder home page

--- app/page.js
+++ app/page.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Navigation from '@/components/navigation/page';
 import Image from "next/image";
 import construction from "../public/images/construction.png";
-// import { construction } from '@public/images/construction.png';
+import  Home  from './home/page';
 
 export default function page() {
   return (
     <div className=' bg-white'>
       <Navigation />
+      <Home/>
       <div className='flex flex-col items-center justify-center h-screen'>
         <Image src={construction} width="700" height="1000" />
         <p className='text-5xl text-[#2c2c2c6e] font-bold mt-8'>COMING SOON</p>
