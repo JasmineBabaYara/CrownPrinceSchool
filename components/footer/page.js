656725ac266082095af0b51765c32e786e482f2c
JasmineBabaYara <babayarajasmine@gmail.com>
2025-04-27 17:40:45 +0000

    add icons

--- components/footer/page.js
+++ components/footer/page.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 // import logo from "../../public/images/logo.jpg";
 
 export default function page() {
   return (
     <div className="relative bottom font-sans">
-            <div className="grid lg:grid-cols-3 grid-cols-1 place-items-start bg-white p-20">
+            <div className="grid lg:grid-cols-3 grid-cols-1  bg-white p-20">
                 <div>
                 <Image src="/images/logo.png" alt="logo" width="120" height="120" className='' />
                 <p className="text-black text-xl font-bold font-sans">Crown Prince School</p>
@@ -14,11 +16,15 @@ export default function page() {
                <div className="text-black text-base flex items-center gap-2 mt-1 font-mono"> <p className="font-semibold">Call: </p><a href="tel:+233551507812">+233551507812</a> </div>
                <p className="text-black text-base mt-3 font-mono">Ghana - Kumasi, Buokrom Estate</p>
                 </div>
-                <div>
+                <div className='flex flex-col items-center'>
                   <div className='flex items-center gap-8'></div>
-                  <div className='flex items-center gap-4'></div>
+                  <div className='flex items-center gap-4'>
+                  <FaFacebookSquare size={50} color='blue'/>
+                  <FaInstagramSquare size={50} color='red' />
+                  <FaSquareXTwitter size={50} color='black' />
+                  </div>
                 </div>
-                <div className='flex flex-col gap-2 mt-10'>
+                <div className='flex flex-col items-center gap-2 mt-10 '>
                   <p className='text-xl text-black font-semibold capitalize font-sans'>Qiuck links</p>
                   <Link href="/"  className='text-base text-black capitalize font-mono mt-4'>Home</Link>
                   <Link href="/admissions"  className='text-base text-black capitalize font-mono'>admissions</Link>
