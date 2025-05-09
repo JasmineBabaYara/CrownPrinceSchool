6f02eb6cdc4b642a0f0445bfa7c3ed8617bf742a
JasmineBabaYara <babayarajasmine@gmail.com>
2025-05-09 14:04:50 +0000

    update contact us

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import Image from "next/image";
-// import construction from "../../public/images/construction.png";
 import Navigation from '@/components/navigation/page';
-
+import Footer from "@/components/footer/page";
 export default function page() {
   return (
     <>
  <Navigation />
-    <div className='flex flex-col items-center justify-center h-screen bg-white'>
-       
-            <Image src="/images/construction.png" width="700" height="1000" />
-            <p className='text-5xl text-[#2c2c2c6e] font-bold mt-8'>COMING SOON</p>
-            <p className='text-sm text-[#515151] mt-4'>The page you’re looking for is not available at the moment. Kindly revisit later.</p>
-          </div>
-          </>
+ <section className="sm:pt-28 pt-20 w-full">
+  <p className="bg-[#213e8c] text-center text-white text-6xl font-bold p-16">Contact</p>
+  </section>
+  <section className="flex items-center justify-center gap-4">
+    <div>
+      <p>Get in Touch</p>
+      <p></p>
+    </div>
+    <div className="overflow-hidden rounded-xl border border-light_blue laptop:w-2/3 w-full">
+								<iframe
+									src={`https://www.google.com/maps/embed/v1/place
+  ?key=YOUR_API_KEY
+  &q=Eiffel+Tower,Paris+France`}
+									height="450"
+									style={{ border: '0' }}
+									allowFullscreen=""
+									loading="lazy"
+									className="w-full"></iframe>
+							</div>
+    </section>
+    <Footer />
+  </>
   )
 }
