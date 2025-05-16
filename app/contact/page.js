066a43a318773dcc3be698bd7b19e99cc5eef259
JasmineBabaYara <babayarajasmine@gmail.com>
2025-05-16 14:28:44 +0000

    embed map

--- app/contact/page.js
+++ app/contact/page.js
@@ -17,12 +17,13 @@ export default function page() {
         </div>
         <div className="overflow-hidden rounded-xl border border-light_blue w-full">
           <iframe
-            src={`https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Eiffel+Tower,Paris+France`}
-            height="450"
-            style={{ border: '0' }}
-            allowFullscreen=""
-            loading="lazy"
-            className="w-full"></iframe>
+
+            className="w-full"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3962.291812443224!2d-1.5878732!3d6.7342097!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdb95c0dccd9259%3A0xd6d0c823f4aa8ac5!2sCrown%20Prince%20International%20Schoo!5e0!3m2!1sen!2sgh!4v1747405328879!5m2!1sen!2sgh"
+            width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+            style={{ border: '0' }} >
+          </iframe>
+
         </div>
       </section>
       <Footer />
