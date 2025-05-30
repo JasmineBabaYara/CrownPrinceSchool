e239614bb2c8de8802a4d04a600a0e8b90d3e012
JasmineBabaYara <babayarajasmine@gmail.com>
2025-05-30 08:54:56 +0000

    add content

--- app/about/page.js
+++ app/about/page.js
@@ -2,22 +2,30 @@ import React from 'react';
 import Image from "next/image";
 // import construction from "../../public/images/construction.png";
 import Navigation from '@/components/navigation/page';
+import Footer from '@/components/footer/page';
 
 export default function page() {
   return (
     <>
- <Navigation />
-    <div className='flex flex-col items-center justify-center h-screen bg-white'>
-       
-            <Image src="/images/construction.png" width="700" height="1000" />
-            <p className='text-5xl text-[#2c2c2c6e] font-bold mt-8'>COMING SOON</p>
-            <p className='text-sm text-[#515151] mt-4'>The page you’re looking for is not available at the moment. Kindly revisit later.</p>
-          </div>
-          <p>At Crown Prince School, we are committed to creating a vibrant, nurturing learning environment where every child is encouraged to thrive. Our goal is to ignite a lifelong love for learning by focusing on the unique needs, interests, and potential of each student.
+      <Navigation />
+      <section className="sm:pt-28 pt-20 w-full">
+        <p className="bg-[#213e8c] text-center text-white text-6xl font-bold lg:p-16 p-20">About US</p>
+      </section>
+      <section className='h-full bg-white pl-60 pr-60 pt-10 pb-10'>
+        <p className='text-center text-lg leading-loose text-gray-600'>At Crown Prince School, we are committed to creating a vibrant, nurturing learning environment where every child is encouraged to thrive. Our goal is to ignite a lifelong love for learning by focusing on the unique needs, interests, and potential of each student.
+          <br /><br />
+          We embrace a child-centered approach that supports both academic excellence and personal growth. Through cross-curricular themes and interactive teaching methods, we help learners make meaningful connections across subjects, build critical thinking skills, and develop a deep understanding of the world around them.
 
-We embrace a child-centered approach that supports both academic excellence and personal growth. Through cross-curricular themes and interactive teaching methods, we help learners make meaningful connections across subjects, build critical thinking skills, and develop a deep understanding of the world around them.
+          Our dedicated team of educators works collaboratively to ensure that students are not only prepared for academic success, but also equipped with the confidence, creativity, and character needed to flourish in life. At Crown Prince School, we believe that education should be joyful, purposeful, and transformative."</p>
+      </section>
+      <section>
+        <div className="bg-[#e63888] p-10 text-white text-3xl font-medium text-center">
+          <p>The focus must always be in learning.</p>
 
-Our dedicated team of educators works collaboratively to ensure that students are not only prepared for academic success, but also equipped with the confidence, creativity, and character needed to flourish in life. At Crown Prince School, we believe that education should be joyful, purposeful, and transformative."</p>
-          </>
+<p>The fundamental tool for that is collaboration.</p>
+        </div>
+      </section>
+      <Footer />
+    </>
   )
 }
